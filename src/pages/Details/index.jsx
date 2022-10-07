8a7deff7194fee722f67ecd2badd97458d787326
Jonatas Amorim Silva <jonatas231997@live.com>
2022-10-06 21:43:24 -0300

    Tag components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./styles";
 import { Header } from '../../components/Header'
 import { Button } from "../../components/Button";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 
 export function Details() {
@@ -15,6 +16,11 @@ export function Details() {
         <li> <a href="#">https://www.rocketseat.com.br/</a></li>
         <li><a href="#">https://pt-br.reactjs.org/</a></li>
       </Links>
+    </Section>
+
+    <Section title="Marcadores">
+      <Tag title="express"></Tag>
+      <Tag title="nodejs"></Tag>
     </Section>
 
       <Button title="Voltar"/>
