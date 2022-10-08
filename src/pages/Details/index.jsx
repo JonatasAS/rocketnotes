8bcd37387dbea4981df00202a158a8fc19330df2
Jonatas Amorim Silva <jonatas231997@live.com>
2022-10-07 22:19:29 -0300

    Finalizing details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./styles";
+import { Container, Links, Content } from "./styles";
 
 import { Header } from '../../components/Header'
 import { Button } from "../../components/Button";
@@ -12,21 +12,31 @@ export function Details() {
     <Container>
       <Header />
 
-    <ButtonText title="Excluir nota"></ButtonText>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota"></ButtonText>
 
-    <Section title="Links úteis">
-      <Links>
-        <li> <a href="#">https://www.rocketseat.com.br/</a></li>
-        <li><a href="#">https://pt-br.reactjs.org/</a></li>
-      </Links>
-    </Section>
+          <h1>Introdução ao React</h1>
 
-    <Section title="Marcadores">
-      <Tag title="express"></Tag>
-      <Tag title="nodejs"></Tag>
-    </Section>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum pariatur corporis iste ad similique, cumque libero eveniet quia tenetur suscipit quibusdam error. Amet iusto dolore in mollitia inventore ipsam numquam.
+          </p>
+          
+          <Section title="Links úteis">
+            <Links>
+              <li> <a href="#">https://www.rocketseat.com.br/</a></li>
+              <li><a href="#">https://pt-br.reactjs.org/</a></li>
+            </Links>
+          </Section>
 
-      <Button title="Voltar"/>
+          <Section title="Marcadores">
+            <Tag title="express"></Tag>
+            <Tag title="nodejs"></Tag>
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
