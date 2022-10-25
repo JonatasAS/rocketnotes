3257469c44ea37d4876dc119371b4ce2a41b0538
Jonatas Amorim Silva <jonatas231997@live.com>
2022-10-24 22:23:20 -0300

    Create input component

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -46,11 +46,10 @@ export const Menu = styled.ul`
 `;
 export const Search = styled.div`
   grid-area: search;
-  background: violet;
+  padding: 64px 64px 0;
 `;
 export const Content = styled.div`
   grid-area: content;
-  background: blue;
 `;
 export const NewNote = styled.button`
   grid-area: newnote;
