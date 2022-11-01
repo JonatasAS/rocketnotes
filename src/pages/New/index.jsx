a23bd1c8b1f734969840b49745aac651f9e690e2
Jonatas Amorim Silva <jonatas231997@live.com>
2022-10-31 22:21:02 -0300

    New pages for note

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/Header";
+
+import { Container } from "./styled";
+
+export function New() {
+  return (
+    <Container>
+      <Header></Header>
+    </Container>
+  )
+}
