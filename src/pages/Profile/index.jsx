abbeb22cd567c16b4fa231d2623923969d74d456
Jonatas Amorim Silva <jonatas231997@live.com>
2022-10-31 21:52:42 -0300

    User avatar

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,43 +1,39 @@
-import { Container, Form } from "./styled";
-import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi";
-import { Input } from "../../components/Input";
-import { Button } from "../../components/Button";
+import { Container, Form, Avatar } from "./styled"
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi"
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
 
-export function Profile(){
+export function Profile() {
   return (
     <Container>
       <header>
         <a href="/">
-          <FiArrowLeft/>
+          <FiArrowLeft />
         </a>
       </header>
 
       <Form>
-        <Input
-          placeholder="Nome"
-          type="text"
-          icon={FiUser}
-        />
+        <Avatar>
+          <img
+            src="https://github.com/JonatasAS.png"
+            alt="Foto do usuário"
+          />
 
-        <Input
-          placeholder="E-mail"
-          type="text"
-          icon={FiMail}
-        />
+          <label htmlFor="avatar">
+            <FiCamera />
 
-        <Input
-          placeholder="Senha atual"
-          type="password"
-          icon={FiLock}
-        />
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
+        <Input placeholder="Nome" type="text" icon={FiUser} />
 
-        <Input
-          placeholder="Nova senha"
-          type="password"
-          icon={FiLock}
-        />
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
 
-        <Button title="Salvar"/>
+        <Input placeholder="Senha atual" type="password" icon={FiLock} />
+
+        <Input placeholder="Nova senha" type="password" icon={FiLock} />
+
+        <Button title="Salvar" />
       </Form>
     </Container>
   )
