ad2bd88937a2dbf1ed99f24b429c07559f2f8ba4
Jonatas Amorim Silva <jonatas231997@live.com>
2022-11-08 22:34:22 -0300

    Navegation

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,9 @@
 import { Container, Form, BackgroundImg} from "./styled";
 import { Input } from "../../components/Input";
 import { FiMail, FiLock } from "react-icons/fi";
-import { Button } from "../../components/Button"
+import { Button } from "../../components/Button";
+import { Link } from "react-router-dom";
+
 
 export function SignIn() {
   return (
@@ -26,7 +28,7 @@ export function SignIn() {
 
         <Button title="Entrar" />
 
-        <a href="#">Criar conta</a>
+        <Link to="register">Criar conta</Link>
       </Form>
 
       <BackgroundImg />
